bde82b54f8b06ad12aeeb300fa29463c24a1103b
Beksultan <beka47674@gmail.com>
2024-11-13 21:07:39 +0500

    updated files

---
+++ Hw-11week js2/script.js
@@ -0,0 +1,9 @@
+let studentCount = parseInt(prompt("Студенттер санын енгізіңіз:"));
+let grades = [];
+for (let i = 1; i <= studentCount; i++) {
+    let grade = parseFloat(prompt(i + "-студенттің бағасын енгізіңіз:"));
+    grades.push(grade);
+}
+let sum = grades.reduce((total, grade) => total + grade, 0);
+let average = sum / studentCount;
+alert("Студенттердің бағаларының орташа мәні: " + average.toFixed(2));
